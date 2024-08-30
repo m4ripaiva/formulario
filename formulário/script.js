b689cd6bf89b69014367c999fab74181f08daf0e
m4ripaiva <160244272+m4ripaiva@users.noreply.github.com>
2024-08-30 08:39:05 -0300

    carga Kickoff

---
+++ formulário/script.js
@@ -0,0 +1,11 @@
+function validarformulario() {
+    var nome = document.getElementById("nome").value;
+    var mensagemErro = document.getElementById("mensagemErro");
+
+    if (nome === "") {
+        mensagemErro.innerHTML = "Por favor, preencha o campo de nome.";
+        return false;
+    }
+    
+    return false;
+}
